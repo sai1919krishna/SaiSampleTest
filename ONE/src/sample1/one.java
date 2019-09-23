eb2696e825ceffd839446ed90292971115dd37ce
dell <dell@dell-PC>
2019-09-23 11:45:43 +0530

    First Commit

---
+++ ONE/src/sample1/one.java
@@ -0,0 +1,19 @@
+package sample1;
+
+public class one {
+
+	public static void main(String[] args) {
+
+		String s1="abcdefg";
+		String s2="abdsajd as fbkjaf lsfnlanf ";
+		/*System.out.println(s1.toUpperCase());
+		System.out.println(s1.replace('a','z'));*/
+		System.out.println(s2.trim());
+		
+		
+		
+		
+		
+	}
+
+}
