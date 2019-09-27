dae6ce2eefb44b930821fbf20a2bf17c83742c7d
dell <you@example.com>
2019-09-27 11:32:52 +0530

    changes made

---
+++ ONE/src/sample1/one2.java
@@ -0,0 +1,22 @@
+package sample1;
+
+public class one2 {
+
+	public static void main(String[] args) {
+
+		int a=3;
+		int b=4;
+		int c=a-b;
+		System.out.println("addition="+c);
+		
+				
+		
+		System.out.println("sai");
+		
+		
+		
+		
+		
+	}
+
+}
